95635590506d4603531d293bab89d3b107827b39
Gabriel Budke <gabrielbudke@gmail.com>
2019-10-06 14:29:13 -0300

    Desenvolvimento do app mobile

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, Image, AsyncStorage, StyleSheet  } from 'react-native';
+
+import logo from '../assets/logo.png'
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+  
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: 'center',
+    marginTop: 30
+  }
+});
